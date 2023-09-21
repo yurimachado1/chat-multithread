0aa7b4c97893e5b71ce0aa6a4d019b7ac95d253a
yurimachado1 <yurimachadomi@gmail.com>
2023-09-20 22:32:52 -0300

    update client.py

--- client.py
+++ client.py
@@ -2,13 +2,13 @@ import socket
 import sys
 import threading
 
-# Função para lidar com a entrada do usuário em uma thread separada
+
 def user_input_thread():
     while True:
         message = input("Digite sua mensagem: ")
         server.send(message.encode("utf-8"))
 
-# Defina o endereço IP do servidor e a porta manualmente
+
 ip_address = '192.168.0.151'
 port = 4040
 
@@ -18,13 +18,13 @@ try:
     print("Conexão com o servidor estabelecida com sucesso!")
 except Exception as e:
     print("Erro ao conectar ao servidor:", e)
-    sys.exit(1)  # Encerra o programa em caso de erro de conexão
+    sys.exit(1)  
 
-# Inicie uma thread para lidar com a entrada do usuário
+
 user_input_thread = threading.Thread(target=user_input_thread)
 user_input_thread.start()
 
-# Loop principal do cliente para receber mensagens
+
 while True:
     try:
         message = server.recv(2048).decode("utf-8")
