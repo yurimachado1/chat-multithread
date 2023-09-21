7ed538916c6c91d6f3e36ad1252d717f79a6ea17
yurimachado1 <yurimachadomi@gmail.com>
2023-09-20 22:24:42 -0300

    update server.py

--- server.py
+++ server.py
@@ -2,106 +2,107 @@ import socket
 import select
 import sys
 import threading
-import os
-
-# Lista para armazenar as mensagens
-messages = []
-
-# Lista para armazenar os arquivos no servidor
-server_files = {}
-
-# Função para enviar mensagens para todos os clientes
-def broadcast(message, client_socket):
-    for client in clients:
-        if client != client_socket:
-            try:
-                client.send(message)
-            except:
-                client.close()
-                remove(client)
-
-# Função para remover um cliente desconectado
-def remove(client):
-    if client in clients:
-        clients.remove(client)
-
-# Função para ordenar e mostrar as últimas 15 mensagens
-def send_recent_messages(client_socket):
-    recent_messages = messages[-15:]
-    for message in recent_messages:
-        client_socket.send(message.encode("utf-8"))
-
-# Função para fazer upload de um arquivo para o servidor
-def upload_file(client_socket, file_name):
-    file_data = client_socket.recv(1024)
-    with open(file_name, "wb") as file:
-        while file_data:
-            file.write(file_data)
-            file_data = client_socket.recv(1024)
-    server_files[file_name] = True
-
-# Função para fazer download de um arquivo do servidor
-def download_file(client_socket, file_name):
-    if file_name in server_files:
-        with open(file_name, "rb") as file:
-            file_data = file.read(1024)
-            while file_data:
-                client_socket.send(file_data)
-                file_data = file.read(1024)
-    else:
-        client_socket.send("Arquivo não encontrado no servidor.".encode("utf-8"))
-
-# Configuração do servidor
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-port = int(sys.argv[1]) if len(sys.argv) > 1 else 19000
-server.bind(('0.0.0.0', port))
-server.listen(5)
+from collections import deque
 
 # Lista de clientes conectados
-clients = []
+client_list = []
 
-print("Servidor de chat está ativo na porta", port)
+# Lista das últimas mensagens
+last_messages = deque(maxlen=15)
 
-# Função para lidar com as mensagens dos clientes
-def client_thread(client_socket):
-    client_socket.send("Bem-vindo ao chat. Use @SAIR para sair.\n".encode())
-
-    while True:
-        try:
-            message = client_socket.recv(2048).decode("utf-8")
-            if message:
-                if message == "@SAIR":
-                    remove(client_socket)
-                    client_socket.close()
-                elif message.startswith("@UPLOAD"):
-                    _, file_name = message.split()
-                    upload_file(client_socket, file_name)
-                elif message.startswith("@DOWNLOAD"):
-                    _, file_name = message.split()
-                    download_file(client_socket, file_name)
-                elif message == "@ORDENAR":
-                    send_recent_messages(client_socket)
-                else:
-                    message_to_broadcast = f"::{message}"
-                    messages.append(message_to_broadcast)
-                    if len(messages) > 15:
-                        messages.pop(0)
-                    broadcast(message_to_broadcast.encode(), client_socket)
-            else:
-                remove(client_socket)
+# Função para enviar uma mensagem para todos os clientes conectados
+def broadcast_message(message, sender_socket):
+    for client_socket in client_list:
+        # Não envie a mensagem de volta para o remetente original
+        if client_socket != sender_socket:
+            try:
+                client_socket.send(message.encode("utf-8"))
+            except Exception as e:
+                print("Erro ao enviar mensagem para o cliente:", e)
                 client_socket.close()
-        except Exception as e:
-            print("Erro:", e)
-            continue
+                client_list.remove(client_socket)
+
+# Função para lidar com as mensagens de um cliente
+def handle_client(client_socket, addr):
+    try:
+        while True:
+            message = client_socket.recv(2048).decode("utf-8")
+            if not message:
+                # Cliente desconectado
+                client_socket.close()
+                client_list.remove(client_socket)
+                print(f"Cliente {addr} desconectado.")
+                break
+            elif message.strip().lower() == "@sair":
+                # Cliente solicitou saída
+                client_socket.close()
+                client_list.remove(client_socket)
+                print(f"Cliente {addr} saiu do chat.")
+                broadcast_message(f"Cliente {addr} saiu do chat.", client_socket)
+                break
+            elif message.strip().lower() == "@ordenar":
+                send_ordered_messages(client_socket)
+            elif message.strip().lower() == "@upload":
+                receive_file(client_socket)
+            else:
+                # Exibir a mensagem no terminal do servidor no formato ":: MENSAGEM"
+                print(f"::{message}")
+                last_messages.append(f"::{message}")
+                # Enviar a mensagem para todos os outros clientes
+                broadcast_message(f"::{message}", client_socket)
+    except Exception as e:
+        print(f"Erro ao lidar com o cliente {addr}: {e}")
+        client_socket.close()
+        client_list.remove(client_socket)
+
+# Função para ordenar e enviar as últimas mensagens para um cliente
+def send_ordered_messages(client_socket):
+    ordered_messages = list(last_messages)
+    ordered_messages.reverse()
+    ordered_messages = "\n".join(ordered_messages)
+    client_socket.send(ordered_messages.encode("utf-8"))
+
+# Função para receber um arquivo do cliente
+def receive_file(client_socket):
+    try:
+        file_name = client_socket.recv(2048).decode("utf-8")
+        with open(file_name, "wb") as file:
+            while True:
+                data = client_socket.recv(2048)
+                if not data:
+                    break
+                file.write(data)
+        print(f"Arquivo '{file_name}' recebido com sucesso.")
+    except Exception as e:
+        print(f"Erro ao receber arquivo do cliente: {e}")
+
+# Defina o endereço IP do servidor e a porta manualmente
+ip_address = '192.168.0.151'
+port = 4040
+
+try:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((ip_address, port))
+    server.listen(5)
+    print(f"Servidor escutando em {ip_address}:{port}")
+except Exception as e:
+    print("Erro ao iniciar o servidor:", e)
+    sys.exit(1)  # Encerra o programa em caso de erro de inicialização
 
 # Loop principal do servidor
 while True:
-    client_socket, addr = server.accept()
-    clients.append(client_socket)
-    print(addr[0] + " conectou-se ao servidor")
+    try:
+        conn, addr = server.accept()
+        print(f"Nova conexão de {addr}")
 
-    # Inicia uma nova thread para lidar com o cliente
-    client_thread_handler = threading.Thread(target=client_thread, args=(client_socket,))
-    client_thread_handler.start()
+        # Adicione o cliente à lista de clientes
+        client_list.append(conn)
+
+        # Inicie uma nova thread para lidar com as mensagens do cliente
+        client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+        client_thread.daemon = True  # Defina como daemon
+        client_thread.start()
+    except Exception as e:
+        print("Erro na conexão:", e)
+        continue
